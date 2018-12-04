1a35d2cb550b768b34a5ac8556e4e8fbb6449cae
dan <josh@deltstl.com>
2018-12-03 20:02:54 -0600

    tree

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  $('header h2').trigger('click');
-})
+  // $('.tree-container__tree').animate({
+  //   'height': '100vh',
+  //   'width': '50vw'
+  // }, 2000, 'easeOutBounce');
+  $('.tree-container').animate({
+    'height': '100vh',
+    'width': '100vw',
+    'top': '0'
+  }, 2000)
+});
