3b8e984de81856a328d602b8c33d6a416bff58f7
dan <josh@deltstl.com>
2018-12-04 21:14:21 -0600

    white pan down

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-  setTimeout(bannerAnimation, 1000)
-  $('body').mousemove(function() {
-  })
-});
-
-var bannerAnimation = function() {
+  $('.tree-container').scrollTop(0);
+  $("#tree").on('load', function() {
+    setTimeout(animateTree, 100);
+  }).each(function() {
+    if (this.complete) $(this).trigger('load');
+  });
+})
+var animateTree = function() {
   $('.tree-container').animate({
-    'height': '150vh'
-  }, 2000, 'easeOutBounce')
+    'scrollTop': '950px'
+  }, 3000, 'easeInQuint', function() {
+
+  })
 }
