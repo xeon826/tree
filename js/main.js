c847fa51f44c7151ba0d24ee79e1d5fd2d3ee30f
dan <josh@deltstl.com>
2018-12-06 20:42:17 -0600

    add scale effect

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
   $("#tree").on('load', function() {
     animateTree();
-    // setTimeout(animateTree, 2000);
   }).each(function() {
     if (this.complete) $(this).trigger('load');
   });
 })
 var animateTree = function() {
-  $('.tree-container__tree, #tree--motion-blur').removeClass('animation-paused');
+  $('.tree-container *').removeClass('animation-paused');
   // $('.tree-container__tree').animate('width','150vw');
 }
