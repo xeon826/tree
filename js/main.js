42781fe4e2ab25f26ddf2f38d873042942b0e4e4
dan <josh@deltstl.com>
2018-12-08 18:00:34 -0600

    box fade in

--- js/main.js
+++ js/main.js
@@ -4,11 +4,7 @@ $(document).ready(function() {
   }).each(function() {
     if (this.complete) $(this).trigger('load');
   });
-  // $(window).scroll(function () {
-  //   $('.tree-container').css('margin-bottom', '-'+$('html').scrollTop()/30);
-  // })
 })
 var animateTree = function() {
-  $('.tree-container *').removeClass('animation-paused');
-  // $('.tree-container__tree').animate('width','150vw');
+  // $('.tree-container *').removeClass('animation-paused');
 }
