ff2378083110c23e4eda3e5449dabde37e8ce317
dan <josh@deltstl.com>
2018-12-08 21:27:11 -0600

    fade in boxes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
 $(document).ready(function() {
-  $("#tree").on('load', function() {
-    animateTree();
-  }).each(function() {
+  $("#tree").on('load', animateTree).each(function() {
     if (this.complete) $(this).trigger('load');
   });
+  $("#tree").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", fadeBoxesIn);
+  boxOpacity();
 })
+
+var boxOpacity = function() {
+  var mX, mY, distance;
+  $(document).mousemove(function(e) {
+    mX = e.pageX;
+    mY = e.pageY;
+    $('.box').each(function() {
+      distance = calculateDistance($(this), mX, mY);
+      $(this).css('opacity', 1 - distance / $(window).innerWidth());
+    })
+  });
+}
+
 var animateTree = function() {
-  // $('.tree-container *').removeClass('animation-paused');
+  $('.tree-container *').removeClass('animation-paused');
+}
+
+var fadeBoxesIn = function() {
+  var time = 500;
+  $('.box').each(function(index, obj) {
+    setTimeout(() => $(this).addClass('loaded'), time * index);
+  })
+}
+
+function calculateDistance(elem, mouseX, mouseY) {
+  return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left + (elem.width() / 2)), 2) + Math.pow(mouseY - (elem.offset().top + (elem.height() / 2)), 2)));
 }
