daa08dd760885b85cb0577bde609ae94013a2a88
dan <josh@deltstl.com>
2018-12-15 18:59:15 -0600

    magnifying glass

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
   hamburger();
   parallax();
   detectBrowser();
+  magnifyingGlass();
 })
 
 
@@ -122,8 +123,52 @@ var detectBrowser = () => {
 
   if (isChrome) {
     $('.section__quote').css({
-      'transform':'translate3d(0,0,0)',
-      '-webkit-transform':'translate3d(0,0,0)',
+      'transform': 'translate3d(0,0,0)',
+      '-webkit-transform': 'translate3d(0,0,0)',
     })
   }
+}
+
+var magnifyingGlass = () => {
+  var sub_width = 0;
+  var sub_height = 0;
+  $(".large").css("background", "url('../img/tree--white-blue.png') contain no-repeat");
+
+
+  $(".box--seo").mousemove(function(e) {
+    if (!sub_width && !sub_height) {
+      var image_object = new Image();
+      image_object.src = $(".small").data("src");
+      sub_width = image_object.width;
+      sub_height = image_object.height;
+    } else {
+      var magnify_position = $(this).offset();
+
+      var mx = e.pageX - magnify_position.left;
+      var my = e.pageY - magnify_position.top;
+
+      if (mx < $(this).width() && my < $(this).height() && mx > 0 && my > 0) {
+        $(".large").fadeIn(100);
+      } else {
+        $(".large").fadeOut(100);
+      }
+      if ($(".large").is(":visible")) {
+        var rx = Math.round(mx / $(".small").width() * sub_width - $(".large").width() / 2) * -1;
+        var ry = Math.round(my / $(".small").height() * sub_height - $(".large").height() / 2) * -1;
+
+        var bgp = rx + "px " + ry + "px";
+
+        var px = mx - $(".large").width() / 2;
+        var py = my - $(".large").height() / 2;
+
+        $(".large").css({
+          left: px,
+          top: py,
+          // backgroundPosition: bgp,
+          'background-position-y': px + 'px',
+          'background-position-x': py + 'px'
+        });
+      }
+    }
+  })
 }
